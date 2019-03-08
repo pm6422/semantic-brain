6465b073e34d859c36e05ee38878e585eea79987
louis <louisliu@deepbrain.ai>
2019-03-08 17:35:58 +0800

    refactor

--- src/main/java/org/infinity/semanticbrain/dialog/entity/Slot.java
+++ src/main/java/org/infinity/semanticbrain/dialog/entity/Slot.java
@@ -4,6 +4,7 @@ import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 @ApiModel("意图识别槽位")
 public class Slot implements Serializable {
@@ -25,4 +26,96 @@ public class Slot implements Serializable {
     private int     askPrecedence;
     @ApiModelProperty("是否为必要槽位")
     private boolean required;
+
+    public Slot() {
+    }
+
+    public static Slot of(String code, String name, String value, String type, int askPrecedence, boolean required) {
+        Slot slot = new Slot();
+        slot.setCode(code);
+        slot.setName(name);
+        slot.setValue(value);
+        slot.setType(type);
+        slot.setAskPrecedence(askPrecedence);
+        slot.setRequired(required);
+        return slot;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getAskPrecedence() {
+        return askPrecedence;
+    }
+
+    public void setAskPrecedence(int askPrecedence) {
+        this.askPrecedence = askPrecedence;
+    }
+
+    public boolean isRequired() {
+        return required;
+    }
+
+    public void setRequired(boolean required) {
+        this.required = required;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Slot slot = (Slot) o;
+        return askPrecedence == slot.askPrecedence &&
+                required == slot.required &&
+                Objects.equals(code, slot.code) &&
+                Objects.equals(name, slot.name) &&
+                Objects.equals(value, slot.value) &&
+                Objects.equals(type, slot.type);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, name, value, type, askPrecedence, required);
+    }
+
+    @Override
+    public String toString() {
+        return "Slot{" +
+                "code='" + code + '\'' +
+                ", name='" + name + '\'' +
+                ", value='" + value + '\'' +
+                ", type='" + type + '\'' +
+                ", askPrecedence=" + askPrecedence +
+                ", required=" + required +
+                '}';
+    }
 }
