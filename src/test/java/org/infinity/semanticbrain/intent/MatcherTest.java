acc460fbad077acac710e06edfb437da3aee4474
louis <louisliu@deepbrain.ai>
2019-04-05 20:38:21 +0800

    refactor

--- src/test/java/org/infinity/semanticbrain/intent/MatcherTest.java
+++ src/test/java/org/infinity/semanticbrain/intent/MatcherTest.java
@@ -14,7 +14,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.trie4j.patricia.PatriciaTrie;
 
 import java.lang.reflect.InvocationTargetException;
@@ -22,7 +21,6 @@ import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
 
-@SpringBootTest
 @RunWith(MockitoJUnitRunner.class)
 public class MatcherTest {
 
