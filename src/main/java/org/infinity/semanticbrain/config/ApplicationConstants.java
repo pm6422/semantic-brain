4bf952aa5ac1c0488032adb2dbd86880cffea08d
louis <louisliu@deepbrain.ai>
2019-05-13 19:49:55 +0800

    add swagger

--- src/main/java/org/infinity/semanticbrain/config/ApplicationConstants.java
+++ src/main/java/org/infinity/semanticbrain/config/ApplicationConstants.java
@@ -16,7 +16,9 @@ public final class ApplicationConstants {
     public static final  String   SPRING_PROFILES_ACTIVE    = "spring.profiles.active";
     public static final  String   SPRING_PROFILE_TEST       = "test";
     public static final  String   SPRING_PROFILE_PRODUCTION = "prod";
-    public static final  String[] AVAILABLE_PROFILES        = new String[]{SPRING_PROFILE_TEST, SPRING_PROFILE_PRODUCTION};
+    // Spring profile used to disable swagger
+    public static final  String   SPRING_PROFILE_NO_SWAGGER = "no-swagger";
+    public static final  String[] AVAILABLE_PROFILES        = new String[]{SPRING_PROFILE_TEST, SPRING_PROFILE_PRODUCTION, SPRING_PROFILE_NO_SWAGGER};
     public static final  String   SYSTEM_ACCOUNT            = "system";
     public static final  String   SCHEDULE_LOG_PATTERN      = "########################Schedule executed: {}########################";
     public static final  Locale   SYSTEM_LOCALE             = Locale.SIMPLIFIED_CHINESE;
--- src/main/java/org/infinity/semanticbrain/controller/NluController.java
+++ src/main/java/org/infinity/semanticbrain/controller/NluController.java
@@ -1,5 +1,9 @@
 package org.infinity.semanticbrain.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.infinity.semanticbrain.dialog.entity.Input;
 import org.infinity.semanticbrain.dialog.entity.Output;
 import org.infinity.semanticbrain.service.NluService;
@@ -8,13 +12,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Api(tags = "语义处理")
 @RestController
 public class NluController {
 
     @Autowired
     private NluService nluService;
 
-    @GetMapping(value = "/open-api/semantic-recognize")
+    @ApiOperation(value = "意图识别", notes = "deviceId取值规则：不指定deviceId时使用登录用户名, userIds多值时使用英文逗号分割")
+    @ApiResponses(value = {@ApiResponse(code = 201, message = "处理成功")})
+    @GetMapping(value = "/api/semantic-recognize")
     public ResponseEntity<Output> semanticRecognize() {
         Input input = new Input();
         return ResponseEntity.ok(nluService.recognize(input));
