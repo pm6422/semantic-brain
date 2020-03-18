efa6128142c1b1e264757764d3c032e33b204872
louis <pm6422@126.com>
2020-03-18 18:49:56 +0800

    rename

--- src/test/java/org/infinity/semanticbrain/collections/JmhBenchmark.java
+++ src/test/java/org/infinity/semanticbrain/collections/JmhBenchmark.java
@@ -1,16 +1,16 @@
 package org.infinity.semanticbrain.collections;
 
-//import org.openjdk.jmh.annotations.Benchmark;
-//import org.openjdk.jmh.annotations.BenchmarkMode;
-//import org.openjdk.jmh.annotations.Fork;
-//import org.openjdk.jmh.annotations.Mode;
-//
-//@BenchmarkMode(Mode.Throughput)
-//public class JmhBenchmark {
-//
-//       @Benchmark
-//       @Fork(3)
-//       public static double benchmarkPerformanceCriticalComponent() {
-//             return 1L;
-//       }
-//}
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Mode;
+
+@BenchmarkMode(Mode.Throughput)
+public class JmhBenchmark {
+
+       @Benchmark
+       @Fork(3)
+       public static double benchmarkPerformanceCriticalComponent() {
+             return 1L;
+       }
+}
---
+++ src/test/java/org/infinity/semanticbrain/collections/JmhBenchmarkTests.java
@@ -0,0 +1,41 @@
+package org.infinity.semanticbrain.collections;
+
+import org.junit.Test;
+import org.openjdk.jmh.results.RunResult;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.text.DecimalFormat;
+import java.util.Collection;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class JmhBenchmarkTests {
+
+    private static       DecimalFormat df              = new DecimalFormat("0.000");
+    private static final double        REFERENCE_SCORE = 37.132;
+
+    @Test
+    public void runJmhBenchmark() throws RunnerException {
+        Options opt = new OptionsBuilder()
+                .include(JmhBenchmark.class.getSimpleName())
+                .build();
+        Collection<RunResult> runResults = new Runner(opt).run();
+        assertFalse(runResults.isEmpty());
+        for (RunResult runResult : runResults) {
+            assertDeviationWithin(runResult, REFERENCE_SCORE, 0.05);
+        }
+    }
+
+    private static void assertDeviationWithin(RunResult result, double referenceScore, double maxDeviation) {
+        double score = result.getPrimaryResult().getScore();
+        double deviation = Math.abs(score / referenceScore - 1);
+        String deviationString = df.format(deviation * 100) + "%";
+        String maxDeviationString = df.format(maxDeviation * 100) + "%";
+        String errorMessage = "Deviation " + deviationString + " exceeds maximum allowed deviation " + maxDeviationString;
+        assertTrue(errorMessage, deviation < maxDeviation);
+    }
+}
