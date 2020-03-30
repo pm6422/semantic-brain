0027617f58d61343175ee38b782f4bc13ae22173
louis <pm6422@126.com>
2020-03-30 16:02:11 +0800

    upgrade to springboot 2.2.5.RELEASE

--- src/main/java/org/infinity/semanticbrain/config/ApplicationProperties.java
+++ src/main/java/org/infinity/semanticbrain/config/ApplicationProperties.java
@@ -21,10 +21,8 @@ public class ApplicationProperties {
     private final Directory            directory            = new Directory();
     private final Async                async                = new Async();
     private final Swagger              swagger              = new Swagger();
-    private final Metrics              metrics              = new Metrics();
     private final Zookeeper            zookeeper            = new Zookeeper();
     private final Redis                redis                = new Redis();
-    private final Logging              logging              = new Logging();
     private final Ribbon               ribbon               = new Ribbon();
     private final HttpClientConnection httpClientConnection = new HttpClientConnection();
     private final Url                  url                  = new Url();
@@ -43,20 +41,12 @@ public class ApplicationProperties {
         return swagger;
     }
 
-    public Metrics getMetrics() {
-        return metrics;
-    }
-
     public Zookeeper getZookeeper() {
         return zookeeper;
     }
 
     public Redis getRedis() {
         return redis;
-    }
-
-    public Logging getLogging() {
-        return logging;
     }
 
     public Ribbon getRibbon() {
@@ -293,145 +283,6 @@ public class ApplicationProperties {
         }
     }
 
-    public static class Metrics {
-
-        private final Jmx jmx = new Jmx();
-
-        private final Spark spark = new Spark();
-
-        private final Graphite graphite = new Graphite();
-
-        private final Logs logs = new Logs();
-
-        public Jmx getJmx() {
-            return jmx;
-        }
-
-        public Spark getSpark() {
-            return spark;
-        }
-
-        public Graphite getGraphite() {
-            return graphite;
-        }
-
-        public Logs getLogs() {
-            return logs;
-        }
-
-        public static class Jmx {
-
-            private boolean enabled = true;
-
-            public boolean isEnabled() {
-                return enabled;
-            }
-
-            public void setEnabled(boolean enabled) {
-                this.enabled = enabled;
-            }
-        }
-
-        public static class Spark {
-
-            private boolean enabled = false;
-
-            private String host = "localhost";
-
-            private int port = 9999;
-
-            public boolean isEnabled() {
-                return enabled;
-            }
-
-            public void setEnabled(boolean enabled) {
-                this.enabled = enabled;
-            }
-
-            public String getHost() {
-                return host;
-            }
-
-            public void setHost(String host) {
-                this.host = host;
-            }
-
-            public int getPort() {
-                return port;
-            }
-
-            public void setPort(int port) {
-                this.port = port;
-            }
-        }
-
-        public static class Graphite {
-
-            private boolean enabled = false;
-
-            private String host = "localhost";
-
-            private int port = 2003;
-
-            private String prefix = "";
-
-            public boolean isEnabled() {
-                return enabled;
-            }
-
-            public void setEnabled(boolean enabled) {
-                this.enabled = enabled;
-            }
-
-            public String getHost() {
-                return host;
-            }
-
-            public void setHost(String host) {
-                this.host = host;
-            }
-
-            public int getPort() {
-                return port;
-            }
-
-            public void setPort(int port) {
-                this.port = port;
-            }
-
-            public String getPrefix() {
-                return prefix;
-            }
-
-            public void setPrefix(String prefix) {
-                this.prefix = prefix;
-            }
-        }
-
-        public static class Logs {
-
-            private boolean enabled = false;
-
-            private int reportFrequency = 60;
-
-            public int getReportFrequency() {
-                return reportFrequency;
-            }
-
-            public void setReportFrequency(int reportFrequency) {
-                this.reportFrequency = reportFrequency;
-            }
-
-            public boolean isEnabled() {
-                return enabled;
-            }
-
-            public void setEnabled(boolean enabled) {
-                this.enabled = enabled;
-            }
-        }
-    }
-
     public static class Dubbo {
 
         private String adminUrl;
@@ -478,60 +329,6 @@ public class ApplicationProperties {
         public void setAdminUrl(String adminUrl) {
             this.adminUrl = adminUrl;
         }
-    }
-
-
-    public static class Logging {
-
-        private final Logstash logstash = new Logstash();
-
-        public Logstash getLogstash() {
-            return logstash;
-        }
-
-        public static class Logstash {
-
-            private boolean enabled = false;
-
-            private String host = "localhost";
-
-            private int port = 5000;
-
-            private int queueSize = 512;
-
-            public boolean isEnabled() {
-                return enabled;
-            }
-
-            public void setEnabled(boolean enabled) {
-                this.enabled = enabled;
-            }
-
-            public String getHost() {
-                return host;
-            }
-
-            public void setHost(String host) {
-                this.host = host;
-            }
-
-            public int getPort() {
-                return port;
-            }
-
-            public void setPort(int port) {
-                this.port = port;
-            }
-
-            public int getQueueSize() {
-                return queueSize;
-            }
-
-            public void setQueueSize(int queueSize) {
-                this.queueSize = queueSize;
-            }
-        }
-
     }
 
     public static class Ribbon {
